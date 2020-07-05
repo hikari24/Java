ee59530fc117778c28dbc1b6c3681b22e819a639
松山ひかり <a.hikari24@gmai.com>
2020-07-05 22:59:20 +0900

    add

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 class Main {
 	public static void main(String args[]) {
 	System.out.println("Hello");
+
+	int number = 10;
+	System.out.println(number);
+
+	String name = "sato";
+	System.out.println(name);
+
+	int i = 1;
+	while(i <= 5) {
+		System.out.println(i);
+		i++;
 	}
+	
+	}
+
 }
